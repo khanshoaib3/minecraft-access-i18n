d6703836ad29bb8d3b9556857b8c84e13a1d01fe
boholder <bottleholder@anche.no>
2024-04-28 16:57:23 +0800

    feat: generate untranslated file

--- .ci/tool.sh
+++ .ci/tool.sh
@@ -34,7 +34,7 @@ diff_keys_between_two_json_files() {
     #  "minecraft_access.area_map.cursor_reach_bound",
     #  "minecraft_access.area_map.cursor_reset",
     #  ...
-    comm -23 <(jq 'keys' $1) <(jq 'keys' $2) >$f
+    comm -23 <(jq -S 'keys' $1) <(jq -S 'keys' $2) >$f
     # remove two white spaces, commas and quotation marks:
     #
     #minecraft_access.area_map.cursor_reach_bound
@@ -58,15 +58,14 @@ work_on_one_language() {
     i18n_file=$1
     name=$(i18n_name "$i18n_file")
     key_file=${TEMP_DIR}/${name}_keys
-    untrans_file=${TEMP_DIR}/${name}_untrans.txt
+    untrans_file=${NOT_TRNAS_DIR}/${name}_untransalted.txt
 
     generate_untrans_key_file $i18n_file $key_file
     if [ $? -eq 1 ]; then
-        rm -f $key_file
         return 0
     fi
 
-    # generate_untrans_file $key_file $untrans_file
+    generate_untrans_file $key_file $untrans_file
 }
 
 generate_untrans_key_file() {
@@ -82,9 +81,25 @@ generate_untrans_key_file() {
     fi
 }
 
+generate_untrans_file() {
+    key_file=$1
+    untrans_file=$2
+
+    touch $untrans_file
+    while read -r key; do
+        # query English text of this field
+        en_value=$(jq -r --arg keyvar "$key" '.[$keyvar]' $EN_I18N_FILE)
+        # append three lines:
+        # {key}
+        # {en_value}
+        # (empty line)
+        printf "%s\n%s\n\n" "$key" "$en_value" >>$untrans_file
+    done <$key_file
+}
+
 # -- main --
 
-rm_rf_directories_if_exists $TEMP_DIR
+rm_rf_directories_if_exists $TEMP_DIR $NOT_TRNAS_DIR
 create_directories_if_not_exists $TEMP_DIR $NEW_TRANS_DIR $NOT_TRNAS_DIR
 work_on_each_language_except_en
-# rm_rf_directories_if_exists $TEMP_DIR
+rm_rf_directories_if_exists $TEMP_DIR
